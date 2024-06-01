cf035c070f471547e286823f2e541415e60a39f8
Geoffrey <geoffreyplichard@gmail.com>
2024-06-01 14:09:58 +0200

    add concat map

--- 4 - Programmation Reactive/RxJS/operators/concatMap.js
+++ 4 - Programmation Reactive/RxJS/operators/concatMap.js
@@ -1,1 +1,27 @@
+// concatMap permet de...
 
+
+// Prenons cet example:
+// Le principe est de sauvegarder les données chaque fois que le formulaire
+// change en envoyant une requête PUT au serveur.
+// Ici il y a 2 problèmes:
+// Le 1er est l'imbrication de subscribe qui est un anti-pattern et doit être évité.
+// 
+
+this.form.valuesChanges
+  .pipe(
+    filter(() => this.form.valid)
+  )
+  .subscribe(changes => {
+
+    const saveCourse$ = fromPromise(fetch(`/api/courses/${this.course.id}`, {
+      method: 'PUT',
+      body: JSON.stringify(changes),
+      header: {
+        'content-type': 'application/json'
+      }
+    
+    saveCourse$.subscribe();
+
+    }));
+  });
