ee03c13bd7396508d653188d1d6f4cf5b70782fe
Geoffrey <geoffreyplichard@gmail.com>
2024-06-01 15:13:34 +0200

    init angular

---
+++ 5 - Frameworks/Angular/demo-app/src/app/home/home.component.ts
@@ -0,0 +1,16 @@
+import { HttpClient } from '@angular/common/http';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss'
+})
+export class HomeComponent {
+  constructor(private http: HttpClient) {
+    console.log('coucou')
+  }
+
+}
