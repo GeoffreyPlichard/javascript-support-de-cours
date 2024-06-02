821e90fff023b1f05ec0fb445b8d75ea5ed679b3
Geoffrey <geoffreyplichard@gmail.com>
2024-06-02 10:09:44 +0200

    implement observable in home

--- 5 - Frameworks/Angular/demo-app/src/app/home/home.component.ts
+++ 5 - Frameworks/Angular/demo-app/src/app/home/home.component.ts
@@ -2,14 +2,13 @@ import {Component, OnInit} from '@angular/core';
 import {Course, sortCoursesBySeqNo} from '../model/course';
 import {interval, noop, Observable, of, throwError, timer} from 'rxjs';
 import {catchError, delay, delayWhen, filter, finalize, map, retryWhen, shareReplay, tap} from 'rxjs/operators';
-import { HttpClient } from '@angular/common/http';
 import {MatDialog, MatDialogConfig} from '@angular/material/dialog';
 import {CourseDialogComponent} from '../course-dialog/course-dialog.component';
 import {MatCardModule} from '@angular/material/card';
 import {MatTabsModule} from '@angular/material/tabs';
 import {MatButtonModule} from '@angular/material/button';
 import { RouterLink } from '@angular/router';
-import { NgFor } from '@angular/common';
+import { AsyncPipe, NgFor } from '@angular/common';
 import { CoursesService } from '../services/courses.service';
 
 @Component({
@@ -17,6 +16,7 @@ import { CoursesService } from '../services/courses.service';
   standalone: true,
   imports: [
     NgFor,
+    AsyncPipe,
     RouterLink,
     MatCardModule, 
     MatTabsModule, 
@@ -27,24 +27,26 @@ import { CoursesService } from '../services/courses.service';
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-  beginnerCourses: Course[] = [];
-  advancedCourses: Course[] = [];
+  beginnerCourses$!: Observable<Course[]>;
+  advancedCourses$!: Observable<Course[]>;
 
   constructor(
-    private http: HttpClient, 
     private dialog: MatDialog,
     private coursesService: CoursesService
   ) {}
 
   ngOnInit() {
-    this.http.get('/api/courses')
-      .subscribe(
-        (res: any) => {
-          const courses: Course[] = res["payload"].sort(sortCoursesBySeqNo);
-          this.beginnerCourses = courses.filter(course => course.category == "BEGINNER");
-          this.advancedCourses = courses.filter(course => course.category == "ADVANCED");
-        });
+    const courses$ = this.coursesService.loadAllCourses()
+      .pipe(
+        map(courses => courses.sort(sortCoursesBySeqNo))
+      );
 
+    this.beginnerCourses$ = courses$.pipe(
+      map(courses => courses.filter(course => course.category === 'BEGINNER'))
+    );
+    this.advancedCourses$ = courses$.pipe(
+      map(courses => courses.filter(course => course.category === 'ADVANCED'))
+    );
   }
 
   editCourse(course: Course) {
