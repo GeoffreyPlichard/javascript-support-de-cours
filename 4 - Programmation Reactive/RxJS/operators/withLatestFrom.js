5c9041237e34a207614d02ec69a3add90bca23a7
Geoffrey <geoffreyplichard@gmail.com>
2024-06-03 11:09:10 +0200

    add withLatestFrom

---
+++ 4 - Programmation Reactive/RxJS/operators/withLatestFrom.js
@@ -0,0 +1,13 @@
+// Permet de combiner plusieurs observable en récupérant les dernières
+// valeurs émises et en les transmettant au next().
+// Retourne un Tuple
+
+// Ici on récupère la dernière valeur de courses$
+this.loadSessons()
+  .pipe(
+    withLatestFrom(this.courses$)
+  )
+  .subscribe(([lessons, courses]) => {
+    console.log('lessons', lessons);
+    console.log('courses', courses);
+  });
