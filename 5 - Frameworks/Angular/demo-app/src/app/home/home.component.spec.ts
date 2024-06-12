b1f73a148d4eb63b1e5673127dd888f8413e6609
Geoffrey <geoffreyplichard@gmail.com>
2024-06-12 09:36:22 +0200

    add component tests

--- 5 - Frameworks/Angular/demo-app/src/app/home/home.component.spec.ts
+++ 5 - Frameworks/Angular/demo-app/src/app/home/home.component.spec.ts
@@ -1,17 +1,23 @@
 import { ComponentFixture, TestBed } from "@angular/core/testing";
 import { HomeComponent } from "./home.component";
 import { DebugElement } from "@angular/core";
-import { CoursesService } from "../services/courses.service";
 import { HttpClientTestingModule } from "@angular/common/http/testing";
 import { CoursesStore } from "../services/courses.store";
 import { LoadingService } from "../loading/loading.service";
 import { MessagesService } from "../messages/messages.service";
+import { setupCourses } from "../common/setup-test-data";
+import { By } from "@angular/platform-browser";
+import { NoopAnimationsModule } from "@angular/platform-browser/animations";
+import { of } from "rxjs";
 
 describe('HomeComponent', () => {
 
   let fixture: ComponentFixture<HomeComponent>;
   let component: HomeComponent;
   let el: DebugElement;
+  let coursesStore: any;
+
+  const beginnerCourses = setupCourses().filter(course => course.category === 'BEGINNER')
 
   beforeEach(async () => {
 
@@ -20,7 +26,8 @@ describe('HomeComponent', () => {
     await TestBed.configureTestingModule({
       imports: [
         HttpClientTestingModule,
-        HomeComponent
+        HomeComponent,
+        NoopAnimationsModule
       ],
       providers: [
         LoadingService,
@@ -35,10 +42,27 @@ describe('HomeComponent', () => {
     fixture = TestBed.createComponent(HomeComponent);
     component = fixture.componentInstance;
     el = fixture.debugElement;
+    coursesStore = TestBed.inject(CoursesStore);
   });
 
   it('should create the component', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should display only beginner courses', () => {
+    
+    // Ici on utilise of() car filterByCategory est censé
+    // retourner un observable
+    // coursesStore.filterByCategory
+    //   .and
+    //   .returnValue(of(beginnerCourses));
+    
+    // Appliquer les data dans le template
+    fixture.detectChanges();
+
+    const tabs = el.queryAll(By.css('.mat-mdc-tab-group'));
+
+    expect(tabs.length).withContext('Unexpected number of tabs').toBe(1);
   });
 
 });
